51aa244fff786fc97fcd2301abab8b7fedce8df5
Thiago Leite <thiagofmleite@gmail.com>
2018-04-03 10:40:18 -0300

    Entendendo o chai

---
+++ tests/main.spec.js
@@ -0,0 +1,44 @@
+var expect = require('chai').expect;
+
+describe('Main', function () {
+  var arr;
+
+  before(function () {
+  });
+
+  after(function () {
+  });
+
+  beforeEach(function () {
+    arr = [1, 2, 3];
+  });
+
+  afterEach(function () {
+  });
+
+
+  it('should be an array', function () {
+    // smoke test
+    expect(arr).to.be.a('array');
+  });
+
+  it('should have a size of 4 when push another value to array', function () {
+    arr.push(4);
+    expect(arr).to.have.lengthOf(4);
+  });
+
+  it('should have a size of 2 when pop a value from the array', function () {
+    arr.pop();
+    expect(arr).to.not.include(3);
+  });
+
+  it('should remove the value 3 when use pop in the array', function () {
+    arr.pop();
+    expect(arr).to.have.lengthOf(2);
+  });
+
+  it('should return true when pop 3 from the array', function () {
+    expect(arr.pop() === 3).to.be.true;
+  });
+
+});
